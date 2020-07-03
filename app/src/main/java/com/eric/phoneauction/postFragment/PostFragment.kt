1c01658614816323f63ed3f5ccdf9fa784e70383
gogo <gogo7751@hotmail.com>
2020-07-03 21:06:03 +0800

    add spinner adapter

--- app/src/main/java/com/eric/phoneauction/postFragment/PostFragment.kt
+++ app/src/main/java/com/eric/phoneauction/postFragment/PostFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import com.eric.phoneauction.PhoneAuctionApplication
 import com.eric.phoneauction.R
+
+
 import com.eric.phoneauction.databinding.PostFragmentBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class PostFragment : Fragment() {
 
@@ -22,8 +27,22 @@ class PostFragment : Fragment() {
 
 
 
+        binding.spinnerBrand.adapter = PostSpinnerAdapter(
+            PhoneAuctionApplication.instance.resources.getStringArray(R.array.brand_list)
+        )
+
+        binding.imageViewPostBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
+        (activity as AppCompatActivity).bottomNavView.visibility = View.GONE
+
         return binding.root
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        (activity as AppCompatActivity).bottomNavView.visibility = View.GONE
+    }
 
 }
